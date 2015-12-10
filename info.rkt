3ac3e6b1c95d3337d1e503eacdfd0c6442f7b732
rafaelreia <rafael.reia@gmail.com>
2015-12-10 12:58:20 +0000

    test

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 ;(define collection "RefactoringTool")
-(define collection 'multi)
+;(define collection 'multi)
 (define drracket-name "Refactoring Tool")
 (define drracket-tools (list (list "refactoringtool.rkt")))
